b2aeb36d939e19882467bdd44815786aeefda149
Qiang Li <joneleesz@hotmail.com>
2017-01-30 21:41:50 +0000

    inital version

---
+++ HealthGreenAssets.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiang
+ * Date: 06/09/16
+ * Time: 3:40 PM
+ */
+
+namespace frontend\web\template\HealthGreen;
+use yii\web\AssetBundle;
+
+
+class HealthGreenAssets extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web/healthgreen/asset';
+
+
+    public $css = [
+        'css/bootstrap.min.css',
+        'css/font-awesome.css',
+        'css/prettyPhoto.css',
+        'css/animate.min.css',
+        'css/responsive.css',
+        'css/scrolling-nav.css',
+        'css/main.css',
+    ];
+
+    public $js = [
+        //'js/jquery.js',
+        'js/bootstrap.min.js',
+        'js/jquery.prettyPhoto.js',
+        'js/jquery.isotope.min.js',
+        //'js/jquery.scrollTo.min.js',
+        'js/wow.min.js',
+        'js/scrolling-nav.js',
+        'js/jquery.easing.min.js',
+        'js/main.js',
+    ];
+
+    public $depends = [
+        // 'yii\web\YiiAsset',
+        'yii\web\JqueryAsset',
+    ];
+}
---
+++ layouts/header.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-fixed-top navbar-default" role="navigation" style="background:#02918c;">
+    <div class="container">
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse" style="border-color: #fff;">
+                <span class="sr-only" style="background-color: #fff">Toggle navigation</span>
+                <span class="icon-bar" style="background-color: #fff"></span>
+                <span class="icon-bar" style="background-color: #fff"></span>
+                <span class="icon-bar" style="background-color: #fff"></span>
+            </button>
+
+            <a class="navbar-brand page-scroll" href="#page-top" style="color: lightgreen;font-size: 2.3rem;">
+                <?=Yii::$app->getTextBlock('logo')->content?>
+            </a>
+        </div>
+
+        <div class="collapse navbar-collapse  navbar-ex1-collapse navbar-right ">
+            <ul class="nav navbar-nav" >
+                <li>
+                    <a href="#service1" class="page-scroll" >Service</a>
+                </li>
+
+                <li>
+                    <a href="#price" class="page-scroll" >Price</a>
+                </li>
+
+                <li>
+                    <a href="#gallery" class="page-scroll" >Gallery</a>
+                </li>
+                <li>
+                    <a href="#contact" class="page-scroll" >Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div><!--/.container-->
+</nav><!--/nav-->
+
+

abeb96c669c3eb7a01377c88be725902cf2cfc8d
Qiang Li <joneleesz@hotmail.com>
2017-03-27 23:57:50 +0100

    change the error page

---
+++ views/error.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiang
+ * Date: 6/10/16
+ * Time: 2:25 PM
+ */
+?>
+
+<section id="error" class="container text-center" style="margin:200px 0px">
+    <h1>We are sorry, something is wrong!!!</h1>
+    <p>The page you access does not exists, please check your url or conact website owner.</p>
+    <a class="btn btn-success" href="<?=Yii::$app->homeUrl?>" style="margin-top: 20px;">Back To Home Page</a>
+</section><!--/#error-->

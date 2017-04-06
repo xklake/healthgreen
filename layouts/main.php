676115fb484b8a0bc1f1bdf4861474b0d3938f51
Qiang Li <joneleesz@hotmail.com>
2017-04-06 19:14:08 +0100

    修改error.php,修改resource的绝对路径

--- layouts/main.php
+++ layouts/main.php
@@ -18,8 +18,8 @@ use yii\helpers\Html;
     <title><?=$this->title?></title>
 
     <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
+    <script src="<?=Yii::$app->urlManager->getHostInfo().'/healthgreen/assets/js/html5shiv.js'?>"></script>
+    <script src="<?=Yii::$app->urlManager->getHostInfo().'/healthgreen/assets/js/respond.min.js'?>"></script>
     <![endif]-->
 
     <?php $this->head() ?>
--- views/error.php
+++ views/error.php
@@ -10,5 +10,7 @@
 <section id="error" class="container text-center" style="margin:200px 0px">
     <h1>We are sorry, something is wrong!!!</h1>
     <p>The page you access does not exists, please check your url or conact website owner.</p>
-    <a class="btn btn-success" href="<?=Yii::$app->homeUrl?>" style="margin-top: 20px;">Back To Home Page</a>
+    <span class="btn btn-success">
+        Back To <a  href="<?=Yii::$app->urlManager->getHostInfo().Yii::$app->homeUrl?>" style="margin-top: 20px;">brighton massage</a>
+    <span>
 </section><!--/#error-->
